0a77060b72849b6fad60721c070d5d72493204b9
itsfeas <amf.fiaz@gmail.com>
2024-02-24 14:16:40 -0700

    tiles

--- client/src/pages/game.tsx
+++ client/src/pages/game.tsx
@@ -5,17 +5,16 @@ const Game: React.FC = () => {
   return (
     <div className="flex bg-blue-400 h-screen">
       <GridComponent
-        board_size={[4, 8]}
+        board_size={[7, 7]}
         grid={{
           tiles: [
-            ["a", "b", "c", "d"],
-            ["e", "f", "g", "h"],
-            ["i", "j", "k", "l"],
-            ["m", "n", "o", "p"],
-            ["a", "b", "c", "d"],
-            ["e", "f", "g", "h"],
-            ["i", "j", "k", "l"],
-            ["m", "n", "o", "p"],
+            ["a", "b", "c", "d", "e", "f", "g"],
+            ["h", "i", "j", "k", "l", "m", "n"],
+            ["o", "p", "q", "r", "s", "t", "u"],
+            ["v", "w", "x", "y", "z", "A", "B"],
+            ["C", "D", "E", "F", "G", "H", "I"],
+            ["J", "K", "L", "M", "N", "O", "P"],
+            ["Q", "R", "S", "T", "U", "V", "W"],
           ],
         }}
       ></GridComponent>
