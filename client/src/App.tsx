484dfad444b2c0d684a853f61b558e89d85d6596
jyw2 <jyuenw@gmail.com>
2024-02-24 13:11:50 -0700

    Co-authored-by: Muhammad Fiaz <itsfeas@users.noreply.github.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Board } from "./data/model";
+import { GridComponent } from "./components/grid/grid";
 
 function App() {
   return (
@@ -19,6 +21,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <GridComponent
+        board_size={[2, 2]}
+        grid={{
+          tiles: [
+            ["a", "b"],
+            ["c", "d"],
+          ],
+        }}
+      ></GridComponent>
     </div>
   );
 }
--- client/src/components/grid/grid.tsx
+++ client/src/components/grid/grid.tsx
@@ -1,5 +1,33 @@
+import { Board } from "../../data/model";
 
-
-export const GridComponent: React.FC = () => {
-    return (<div></div>);
+interface GridComponentProps {
+  grid: Board;
+  board_size: [number, number];
 }
+
+interface TileComponentProps {
+  val: string;
+}
+
+const Tile: React.FC<TileComponentProps> = ({ val }) => {
+  return (
+    <div>
+      <p className="text-bold text-lg">{val}</p>
+    </div>
+  );
+};
+
+export const GridComponent: React.FC<GridComponentProps> = ({
+  grid,
+  board_size,
+}) => {
+  return (
+    <div
+      className={`grid grid-rows-${board_size[0]} grid-cols-${board_size[1]} gap-4`}
+    >
+      {grid.tiles.map((g) => (
+        <Tile />
+      ))}
+    </div>
+  );
+};
