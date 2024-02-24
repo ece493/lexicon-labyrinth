b4663b5f4ec6b4a2c284e22298aed45c96b2d6d8
itsfeas <amf.fiaz@gmail.com>
2024-02-24 13:27:40 -0700

    update styles for main app page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,21 +6,7 @@ import { GridComponent } from "./components/grid/grid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex bg-blue-400 h-screen">
       <GridComponent
         board_size={[2, 2]}
         grid={{
