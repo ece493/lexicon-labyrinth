27df452972fa6921a126328dc67d5274e79b4f53
jyw2 <jyuenw@gmail.com>
2024-03-06 13:44:30 -0700

    Add icons

---
+++ client/src/components/icons/addIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Player } from "../../data/model";
+
+interface AddIconComponentProps {}
+
+const AddIcon: React.FC<AddIconComponentProps> = ({}) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+      className=" fill-slate-100"
+
+    >
+      <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+    </svg>
+  );
+};
+
+export default AddIcon;
