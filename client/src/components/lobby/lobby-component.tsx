8605f29cbb1d88b5f47b572f9d7c8103e3b11771
itsfeas <amf.fiaz@gmail.com>
2024-03-07 10:08:37 -0700

    start game btn

--- client/src/components/lobby/lobby-component.tsx
+++ client/src/components/lobby/lobby-component.tsx
@@ -8,9 +8,14 @@ interface LobbyProps {
 
 const LobbyComponent: React.FC<LobbyProps> = ({lobby, player_id}) => {
     return (
-        <div className="flex justify-center bg-blue-400 h-screen align-middle items-center">
-            <div className="h-64 w-[75vw] bg-blue-500 p-12 rounded-3xl" >
+        <div className="flex flex-col justify-center bg-blue-400 h-screen align-middle items-center">
+            <div className="h-[75vh] w-[75vw] bg-blue-500 p-12 rounded-3xl" >
 
+            </div>
+            <div className="w-[75vw] h-16">
+                <div className="flex flex-auto justify-end h-full py-4">
+                    <button className="bg-red-400 rounded-xl h-10 py-2 px-4 text-pink-200">Start</button>
+                </div>
             </div>
         </div>
     );
