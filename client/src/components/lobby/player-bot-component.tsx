dc3c28ebd118518b27e173dd15c5a8194f0938dd
itsfeas <amf.fiaz@gmail.com>
2024-03-07 15:12:11 -0700

    fix to work w mui

--- client/src/components/lobby/player-bot-component.tsx
+++ client/src/components/lobby/player-bot-component.tsx
@@ -18,9 +18,9 @@ export const BotComponent: React.FC<Bot> = ({...p}) => {
 
 export const BotPlayerComponent: React.FC<PlayerBotComponentProps> = ({name, img}) => {
     return (
-        <div className="flex flex-auto flex-col items-center justify-center h-40 w-32 bg-blue-500 p-4 rounded-3xl" >
-            <img className="h-16 w-16 opacity-80 invert" src={img}></img>
-            <h4 className="text-slate-100 text-center">{name}</h4>
+        <div className="m-0 flex flex-auto flex-col items-center justify-center h-40 w-32 bg-blue-500 p-4 rounded-3xl" >
+            <img className="m-0 h-16 w-16 opacity-80 invert" src={img}></img>
+            <h4 className="m-0 text-slate-100 text-center">{name}</h4>
         </div>
     );
 }
