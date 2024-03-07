c01cebe0861393a05352f30b0030133ab91c0bdb
itsfeas <amf.fiaz@gmail.com>
2024-03-07 09:54:10 -0700

    use filler data

--- client/src/pages/lobby.tsx
+++ client/src/pages/lobby.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import LobbyComponent from "../components/lobby/lobby-component";
+import { Lobby, Player } from "../data/model";
 
-const Lobby: React.FC = () => {
+const LobbyPage: React.FC = () => {
+  const host: Player = { id: 0, name: "John", is_spectator: false, lives: 3, money: 100 };
+  const lobby: Lobby = {
+    state: {
+      turns: {
+        order: [0],
+        curr_turn: 0
+      },
+      board: {
+        tiles: [["a", "b", "c", "d", "e", "f", "g"],
+        ["h", "i", "j", "k", "l", "m", "n"],
+        ["o", "p", "q", "r", "s", "t", "u"],
+        ["v", "w", "x", "y", "z", "A", "B"],
+        ["C", "D", "E", "F", "G", "H", "I"],
+        ["J", "K", "L", "M", "N", "O", "P"],
+        ["Q", "R", "S", "T", "U", "V", "W"],]
+      },
+      timer: 0,
+      memory: new Set<string>(),
+    },
+    max_lives: 5,
+    host: 0,
+    board_size: [7, 7],
+    timer_setting: 0,
+    lobby_code: "",
+    players: [host]
+  };
   return (
-    <header>
-      <h1>Lobby Page</h1>
-    </header>
+    <LobbyComponent lobby={lobby} player_id={0} />
   );
 };
 
-export default Lobby;
+export default LobbyPage;
