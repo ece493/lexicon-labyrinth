f5f8e80bf537004b4fe8ad56a1071a2dfe8badf2
itsfeas <amf.fiaz@gmail.com>
2024-03-27 13:32:22 -0600

    add defaultlayout + fixed name entry page

--- client/src/components/lobby/name-entry/name-entry-lobby-component.tsx
+++ client/src/components/lobby/name-entry/name-entry-lobby-component.tsx
@@ -1,43 +1,36 @@
 import React, { useContext, useState } from "react";
 import { GameContext } from "../../../context/ctx";
 import { ScreenState } from "../../../data/model";
+import DefaultLayout from "../../layout/default-layout";
+import Button from "@mui/material/Button";
 
 const NameEntryLobbyComponent: React.FC = () => {
     const ctx = useContext(GameContext);
     return (
-        <div className="m-0 h-screen flex flex-col justify-center bg-blue-400 align-middle items-center">
-            <div className="m-0 h-[70vh] w-[75vw] flex flex-row bg-blue-500 p-12 gap-12 rounded-3xl" >
-                <div className="h-auto w-full flex flex-col p-8 bg-blue-400 rounded-3xl justify-center items-center">
-                    <h1 className="text-slate-100">Enter Your Nickname Here</h1>
-                    <input defaultValue={""} onChange={(e) => ctx.setPlayerName(e.target.value)}
-                        className="p-1 text-3xl rounded-2xl"></input>
-                    <button
-                        onClick={() => {
-                            ctx.setScreen(ScreenState.LOBBY_CODE_ENTRY);
-                        }}
-                        className="m-6 bg-red-400 text-lg rounded-2xl h-12 px-6 text-pink-100"
-                        >Start</button>
-                </div>
-            </div>
-        </div>
+        <DefaultLayout>
+            <h1 className="m-0 text-slate-100">Enter Your Nickname Here</h1>
+            <input defaultValue={""} onChange={(e) => ctx.setPlayerName(e.target.value)}
+                className="p-1 text-3xl"></input>
+            <Button
+                onClick={() => ctx.setScreen(ScreenState.LOBBY_CODE_ENTRY)}
+                className={"bg-red-400 h-10 py-2 px-4 text-pink-100"}
+                style={{ textTransform: 'none' }}
+            >Start</Button>
+        </DefaultLayout>
     );
 };
 
 export const JoinLobbyErrorComponent: React.FC<{msg: string}> = ({msg}) => {
     const ctx = useContext(GameContext);
     return (
-        <div className="m-0 h-screen flex flex-col justify-center bg-blue-400 align-middle items-center">
-            <div className="m-0 h-[70vh] w-[75vw] flex flex-row bg-blue-500 p-12 gap-12 rounded-3xl" >
-                <div className="h-auto w-full flex flex-col p-8 bg-blue-400 rounded-3xl justify-center items-center">
+        <DefaultLayout>
                     <h1 className="text-slate-100">{msg}</h1>
-                    <button
+                    <Button
                         onClick={() => ctx.setScreen(ScreenState.LOBBY_CODE_ENTRY)}
-                        className="m-6 bg-red-400 text-lg rounded-2xl h-12 px-6 text-pink-100">
-                        Return
-                        </button>
-                </div>
-            </div>
-        </div>
+                        className={"bg-red-400 h-10 py-2 px-4 text-pink-100"}
+                        style={{ textTransform: 'none' }}
+                    >Return</Button>
+        </DefaultLayout>
     );
 };
 
