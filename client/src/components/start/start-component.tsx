6edaaddacf20f73f3c3d22c8ed3ff1e63bf9c316
itsfeas <amf.fiaz@gmail.com>
2024-03-28 12:06:09 -0600

    start screen animation

--- client/src/components/start/start-component.tsx
+++ client/src/components/start/start-component.tsx
@@ -1,20 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GameContext } from "../../context/ctx";
 import DefaultLayout from "../layout/default-layout";
 import Button from "@mui/material/Button";
 import { TileComponent } from "../grid/tile";
+import { ScreenState } from "../../data/model";
+
+const TileComponentWrapper: React.FC<{value: string}> = ({value}) => {
+    const [empty, setEmpty] =  useState(true);
+    useEffect(() => {
+        const t = setTimeout(() => setEmpty(false), Math.random()*2000 + 1000);
+        return () => clearTimeout(t);
+    }, []);
+    return <TileComponent value={empty?"":value} dark={empty}/>;
+}
 
 const StartComponent: React.FC = () => {
     const ctx = useContext(GameContext);
-    const lst1 = "LEXICON".split('').map((l, i) => <TileComponent key={i} value={l} />);
-    const lst2 = "LABYRINTH".split('').map((l, i) => <TileComponent key={i} value={l} />);
+    const lst1 = "LEXICON".split('').map((l, i) => <TileComponentWrapper key={i} value={l} />);
+    const lst2 = "LABYRINTH".split('').map((l, i) => <TileComponentWrapper key={i} value={l} />);
+
     return (
         <DefaultLayout>
             <div className="flex flex-row gap-2">{lst1}</div>
             <div className="flex flex-row gap-2">{lst2}</div>
             <div className="h-3"></div>
             <Button
-                onClick={() => ctx.sock && ctx.transitions.initialize(ctx)}
+                onClick={() => ctx.setScreen(ScreenState.NAME_ENTRY)}
                 className={"bg-white h-10 py-2 px-4 text-blue-600"}
                 style={{ textTransform: 'none' }}
             >Start</Button>
