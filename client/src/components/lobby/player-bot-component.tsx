cd049d75dc99a1369dd2fb18236207a5b8065cc6
itsfeas <amf.fiaz@gmail.com>
2024-03-31 18:43:17 -0600

    hover response'

--- client/src/components/lobby/player-bot-component.tsx
+++ client/src/components/lobby/player-bot-component.tsx
@@ -9,32 +9,34 @@ import AddIcon from "../icons/addIcon";
 
 type PlayerBotManagerComponentProps = {
     player: Player | Bot,
+    is_player: boolean,
     delete_player?: () => void,
     cycle_difficulty?: () => string,
     toggleManageMode?: () => void,
 }
 
 type PlayerBotDisplayComponentProps = {
+    is_player: boolean,
     name: string,
     img: string,
 }
 
-export const PlayerComponent: React.FC<Player> = ({...p}) => <BotPlayerComponent name={p.name} img={personSvg} />;
-export const BotComponent: React.FC<Bot> = ({...p}) => <BotPlayerComponent name={p.name} img={robotSvg} />;
+export const PlayerComponent: React.FC<Player & {is_player: boolean}> = ({...p}) => <BotPlayerComponent name={p.name} img={personSvg} is_player={p.is_player} />;
+export const BotComponent: React.FC<Bot & { is_player: boolean }> = ({...p}) => <BotPlayerComponent name={p.name} img={robotSvg} is_player={p.is_player} />;
 
-export const BotPlayerComponent: React.FC<PlayerBotDisplayComponentProps> = ({name, img}) => {
+export const BotPlayerComponent: React.FC<PlayerBotDisplayComponentProps> = ({is_player, name, img}) => {
     return (
-        <div className="m-0 flex flex-auto flex-col items-center justify-center h-40 w-32 bg-blue-400 p-4" >
+        <div className={`m-0 flex flex-auto flex-col items-center justify-center h-40 w-32 bg-blue-400 p-4 ${is_player ? "border-white border-2" : ""}`} >
             <img className="m-0 h-16 w-16 opacity-80 invert" src={img}></img>
-            <h4 className="m-0 text-slate-100 text-center">{name}</h4>
+            <h4 className={`m-0 text-slate-100 text-center ${is_player ? "text-red-300" : ""}`}>{name}</h4>
         </div>
     );
 }
 
-export const PlayerBotAdminComponent: React.FC<PlayerBotManagerComponentProps> = ({ delete_player, cycle_difficulty, toggleManageMode }) => {
+export const PlayerBotAdminComponent: React.FC<PlayerBotManagerComponentProps> = ({ is_player, delete_player, cycle_difficulty, toggleManageMode }) => {
     const [difficulty, setDifficulty] = useState<string>("Medium");
     return (
-        <div className="m-0 flex flex-auto flex-col items-center justify-between h-48 w-40 bg-blue-400">
+        <div className={`m-0 flex flex-auto flex-col items-center justify-between h-48 w-40 bg-blue-400 ${is_player?"border-white border-2": ""}`}>
             <div className="w-full h-10 m-0">
                 <IconButton onClick={toggleManageMode} size="medium" className="float-right">
                     <CloseIcon sx={{ color: "white" }} fontSize="inherit" />
@@ -70,17 +72,18 @@ export const PlayerBotAdminComponent: React.FC<PlayerBotManagerComponentProps> =
     );
 }
 
-export const PlayerBotManagerComponent: React.FC<PlayerBotManagerComponentProps> = ({ player, delete_player, cycle_difficulty })=> {
+export const PlayerBotManagerComponent: React.FC<PlayerBotManagerComponentProps> = ({ is_player, player, delete_player, cycle_difficulty })=> {
     const [manageMode, setManageMode] = useState<boolean>(false);
     const toggleManageMode = () => setManageMode(m => !m);
-    return <div className="h-auto w-auto p-0 m-0" onClick={(manageMode) ? () => { } : () => setManageMode(!manageMode)}>{
+    return <div className={`h-auto w-auto p-0 m-0 ${!manageMode && delete_player? "hover:opacity-80": ""}`}
+        onClick={(manageMode) ? () => { } : () => setManageMode(!manageMode)}>{
             (manageMode && delete_player)
-                ? <PlayerBotAdminComponent player={player} delete_player={() => {
+                ? <PlayerBotAdminComponent player={player} is_player={is_player} delete_player={() => {
                     setManageMode(!manageMode);
                     delete_player();
                     }} cycle_difficulty={cycle_difficulty} toggleManageMode={toggleManageMode}/>
                 : (isPlayerABot(player))
-                    ? <BotComponent {...(player as Bot)} />
-                    : <PlayerComponent {...player} />
+                    ? <BotComponent {...(player as Bot)} is_player={is_player} />
+                    : <PlayerComponent {...player} is_player={is_player} />
         }</div>;
 }
