b8003109e3811b6a5e900a4d9459e635ebf42d24
itsfeas <amf.fiaz@gmail.com>
2024-04-01 17:37:17 -0600

    error componenet

---
+++ client/src/components/error/error-component.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { GameContext } from "../../context/ctx";
+import { ScreenState } from "../../data/model";
+import DefaultLayout from "../layout/default-layout";
+import Button from "@mui/material/Button";
+
+
+export const ErrorComponent: React.FC<{ msg: string; screen: ScreenState; }> = ({ msg, screen }) => {
+    const ctx = useContext(GameContext);
+    return (
+        <DefaultLayout>
+            <h1 className="text-slate-100">{msg}</h1>
+            <Button
+                onClick={() => ctx.setScreen(screen)}
+                className={"bg-red-400 h-10 py-2 px-4 text-pink-100"}
+                style={{ textTransform: 'none' }}
+            >Return</Button>
+        </DefaultLayout>
+    );
+};
