ca970d35633a333b5a13820d897f23466b5858be
itsfeas <amf.fiaz@gmail.com>
2024-04-01 17:35:44 -0600

    move err file

--- client/src/pages/join-lobby.tsx
+++ client/src/pages/join-lobby.tsx
@@ -1,5 +1,7 @@
-import JoinLobbyComponent, { JoinLobbyErrorComponent } from "../components/lobby/join/join-lobby-component";
+import JoinLobbyComponent from "../components/lobby/join/join-lobby-component";
+import { ErrorComponent } from "../components/error/error-component";
+import { ScreenState } from "../data/model";
 
 const JoinLobbyPage = () => <JoinLobbyComponent />;
-export const JoinLobbyErrorPage = (msg: string) => <JoinLobbyErrorComponent msg={msg}/>;
+export const JoinLobbyErrorPage = (msg: string) => <ErrorComponent msg={msg} screen={ScreenState.LOBBY_CODE_ENTRY}/>;
 export default JoinLobbyPage;
